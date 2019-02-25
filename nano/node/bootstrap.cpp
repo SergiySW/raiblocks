261005457fc92e8f69c8def310838c7de730c186
SergiySW <sergiysw@gmail.com>
2019-02-26 00:47:24 +0300

    Limit maximum length of the queue of pending incoming connections for bootstrap listener

--- nano/node/bootstrap.cpp
+++ nano/node/bootstrap.cpp
@@ -1941,6 +1941,7 @@ void nano::bootstrap_listener::start ()
 {
 	acceptor.open (local.protocol ());
 	acceptor.set_option (boost::asio::ip::tcp::acceptor::reuse_address (true));
+	acceptor.max_listen_connections = node.config.bootstrap_connections_max;
 
 	boost::system::error_code ec;
 	acceptor.bind (local, ec);
