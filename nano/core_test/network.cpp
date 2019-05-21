1a25bf324ffbf55c3da0ad6a33543cb427aabfda
SergiySW <sergiysw@gmail.com>
2019-05-21 12:10:07 +0300

    Fix confirmation_height.conflict_rollback_cemented

--- nano/core_test/network.cpp
+++ nano/core_test/network.cpp
@@ -2086,7 +2086,7 @@ TEST (confirmation_height, conflict_rollback_cemented)
 	}
 
 	auto rollback_log_entry = boost::str (boost::format ("Failed to roll back %1%") % send2->hash ().to_string ());
-	system.deadline_set (10s);
+	system.deadline_set (20s);
 	auto done (false);
 	while (!done)
 	{
